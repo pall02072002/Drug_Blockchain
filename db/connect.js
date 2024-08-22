b5851c80311d08e756dfac554bb6cbb24a9759c5
TANISHKA JAYESH DAS <147196596+tanishka786@users.noreply.github.com>
2024-08-22 20:23:39 +0530

    made few changes

---
+++ db/connect.js
@@ -0,0 +1,12 @@
+const mongoose = requires("mongoose");
+
+uri = "mongodb+srv://tanishka786:<aCrZVREx2FZXlif5>@drugblock.q7h2t.mongodb.net/";
+const connectDB = () => {
+    return mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+};
+
+module.exports = connectDB;
+
